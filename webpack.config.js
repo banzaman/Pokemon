7fd14e2986eb41ea6ee757546a3e9b3ed2d2558b
MARK RUBANZA <markrubanza10@gmail.com>
2023-02-12 01:02:35 +0300

    error

--- webpack.config.js
+++ webpack.config.js
@@ -12,8 +12,7 @@ module.exports = {
   },
   plugins: [
     new HtmlWebpackPlugin({
-      title: 'Output Management',
-      // template: './src/index.html',
+      template: './src/index.html',
     }),
   ],
   output: {
